1294a24b51df02fa18816797b3c7e1f04dfda088
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-09-01 10:57:53 +0200

    init

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi2.Models;
+namespace WebApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        List<Product> products = new List<Product>()
+        {
+            new Product
+            {
+            Name ="zahra kabdoli1",
+            Id =1,
+            Description="pretty girls",
+            Price=11
+            },
+            new Product
+            {
+            Name ="zahra kabdoli2",
+            Id =1,
+            Description="pretty girls",
+            Price=11
+             },
+
+            new Product
+            {
+            Name ="zahra kabdoli3",
+            Id =1,
+            Description="pretty girls",
+            Price=11
+            },
+           new Product
+            {
+            Name ="zahra kabdoli1",
+            Id =1,
+            Description="pretty girls",
+            Price=11
+            },
+            new Product
+            {
+            Name ="zahra kabdoli1",
+            Id =1,
+            Description="pretty girls",
+            Price=11
+            },
+            new Product
+            {
+            Name ="zahra kabdoli1",
+            Id =1,
+            Description="pretty girls",
+            Price=11
+            }
+
+        };
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+        [HttpGet("{id}")]
+        //lab 3 working
+        //test
+    }
+}
